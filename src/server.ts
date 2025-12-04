aa6525f5c57e5d478068c1921a46d6cec3d1b0ba
unknown <sadiksourov11@gmail.com>
2025-12-05 01:18:24 +0600

    user table created

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,55 @@
-import express, { Request, Response } from 'express'
-const app = express()
-const port = 5000
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+import path from "path";
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Universe..!')
-})
+const app = express();
+const port = 5000;
 
-app.post('/', (req: Request, res: Response) => {
+// parser
+app.use(express.json());
+
+// dotenv
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+// DB
+const pool = new Pool({
+  connectionString: `${process.env.DB_CONNECTION_STR}`,
+});
+
+const connectDB = async () => {
+  await pool.query(`   
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+
+            name VARCHAR(100) NOT NULL,
+
+            email VARCHAR(200) UNIQUE NOT NULL
+             CHECK (email = LOWER(email)),
+
+            password VARCHAR(200) NOT NULL
+             CHECK (LENGTH(password) >= 6),
+
+            phone VARCHAR(20) NOT NULL,
+
+            role VARCHAR(20) NOT NULL
+             CHECK (role IN ('admin', 'customer'))
+             )`);
+    };
+
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Universe..!!");
+});
+
+app.post("/", (req: Request, res: Response) => {
   res.status(201).json({
-  success: true,
-  message: 'Api is working fine'
-})      
+    success: true,
+    message: "Api is working fine",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
